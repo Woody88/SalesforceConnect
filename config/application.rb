0d19b3ae1d394c978001fea82a39e1ccd845baa7
Woodson Delhia <woodsondelhia88@mail.com>
2017-05-31 16:25:07 -0400

    added eager load path for lib folder to load in prod

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module SalesforceConnect
     config.sass.preferred_syntax = :sass
     config.sass.line_comments = false
     config.sass.cache = false
+    config.eager_load_paths << "#{Rails.root}/lib"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
